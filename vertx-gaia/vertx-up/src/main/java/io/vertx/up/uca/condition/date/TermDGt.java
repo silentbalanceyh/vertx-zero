a3b2600362cbb4acd5b90db1746579cf3f06d216
Lang <silentbalanceyh@126.com>
2020-04-15 21:13:35 +0800

    Push

--- vertx-gaia/vertx-up/src/main/java/io/vertx/up/uca/condition/date/TermDGt.java
+++ vertx-gaia/vertx-up/src/main/java/io/vertx/up/uca/condition/date/TermDGt.java
@@ -1,13 +1,14 @@
-package io.vertx.up.uca.condition;
+package io.vertx.up.uca.condition.date;
 
+import io.vertx.up.uca.condition.Term;
 import org.jooq.Condition;
 import org.jooq.Field;
 import org.jooq.impl.DSL;
 
 @SuppressWarnings("all")
-public class TermDLt implements Term {
+public class TermDGt implements Term {
     @Override
     public Condition where(final Field field, final String fieldName, final Object value) {
-        return DSL.field(fieldName).lt(value);
+        return DSL.field(fieldName).gt(value);
     }
 }
--- vertx-gaia/vertx-up/src/main/java/io/vertx/up/uca/condition/date/TermDLt.java
+++ vertx-gaia/vertx-up/src/main/java/io/vertx/up/uca/condition/date/TermDLt.java
@@ -1,13 +1,14 @@
-package io.vertx.up.uca.condition;
+package io.vertx.up.uca.condition.date;
 
+import io.vertx.up.uca.condition.Term;
 import org.jooq.Condition;
 import org.jooq.Field;
 import org.jooq.impl.DSL;
 
 @SuppressWarnings("all")
-public class TermDGt implements Term {
+public class TermDLt implements Term {
     @Override
     public Condition where(final Field field, final String fieldName, final Object value) {
-        return DSL.field(fieldName).gt(value);
+        return DSL.field(fieldName).lt(value);
     }
 }
