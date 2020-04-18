cbe849d4149a5c07b985ac901117c9530d5a356d
Lang <silentbalanceyh@126.com>
2020-04-18 11:11:15 +0800

    Add freedom mode

---
+++ vertx-gaia/vertx-up/src/main/java/io/vertx/up/uca/rs/hunt/adaptor/WingSelector.java
@@ -0,0 +1,52 @@
+package io.vertx.up.uca.rs.hunt.adaptor;
+
+import io.vertx.up.log.Annal;
+
+import javax.ws.rs.core.MediaType;
+import java.util.Objects;
+import java.util.Set;
+import java.util.concurrent.ConcurrentMap;
+
+/**
+ * @author <a href="http://www.origin-x.cn">lang</a>
+ */
+public class WingSelector {
+
+    public static Wings end(final String contentType, final Set<MediaType> produces) {
+        /*
+         * content type
+         * accept
+         */
+        final MediaType type;
+        if (Objects.isNull(contentType)) {
+            /*
+             * Default String mode
+             *
+             * 1. Content-Type is `* / *` format
+             * 2. Replied body directly
+             */
+            type = MediaType.WILDCARD_TYPE;
+        } else {
+            /*
+             * Extract the data `MediaType` from response header
+             */
+            type = MediaType.valueOf(contentType);
+        }
+        /*
+         * 1. type for first level
+         * 2. subtype for second level
+         */
+        final ConcurrentMap<String, Wings> subtype = Pool.SELECT_POOL.get(type.getType());
+        final Wings selected;
+        if (Objects.isNull(subtype) || subtype.isEmpty()) {
+            selected = new JsonWings();
+        } else {
+            final Wings wings = subtype.get(type.getSubtype());
+            selected = Objects.isNull(wings) ? new JsonWings() : wings;
+        }
+        final Annal logger = Annal.get(WingSelector.class);
+        logger.info("Wings response selected `{0}` for content type {1}",
+                selected.getClass().getName(), contentType);
+        return selected;
+    }
+}
