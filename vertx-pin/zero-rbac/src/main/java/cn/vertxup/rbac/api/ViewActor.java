c5ebbd984d6fe8d641b6e932f1cf31ec2ce9a0ae
HuaRui <evgreen@126.com>
2020-06-28 23:42:07 +0800

    clear habit after modify permissions

--- vertx-pin/zero-rbac/src/main/java/cn/vertxup/rbac/api/ViewActor.java
+++ vertx-pin/zero-rbac/src/main/java/cn/vertxup/rbac/api/ViewActor.java
@@ -3,9 +3,12 @@ package cn.vertxup.rbac.api;
 import cn.vertxup.rbac.service.view.ViewStub;
 import io.vertx.core.Future;
 import io.vertx.core.json.JsonObject;
+import io.vertx.tp.ke.refine.Ke;
 import io.vertx.tp.rbac.cv.Addr;
 import io.vertx.up.annotations.Address;
 import io.vertx.up.annotations.Queue;
+import io.vertx.up.commune.Envelop;
+import io.vertx.up.unity.Ux;
 
 import javax.inject.Inject;
 
@@ -16,7 +19,11 @@ public class ViewActor {
     private transient ViewStub viewStub;
 
     @Address(Addr.View.VIEW_UPDATE_BY_TYPE)
-    public Future<JsonObject> updateViewByType(final String ownerType, final String key, final JsonObject data) {
-        return viewStub.updateByType(ownerType, key, data);
+    public Future<JsonObject> updateViewByType(final Envelop envelop) {
+        final String ownerType = Ux.getString(envelop).toUpperCase();
+        final String key       = Ux.getString1(envelop);
+        final JsonObject data  = Ux.getJson2(envelop);
+        final String habit     = Ke.keyHabitus(envelop);
+        return viewStub.updateByType(ownerType, key, data, habit);
     }
 }
