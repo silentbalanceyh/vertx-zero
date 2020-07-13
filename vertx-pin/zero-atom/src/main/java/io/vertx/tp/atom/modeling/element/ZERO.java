2bfe52032bf785b391c88799ac6dcb5d18b2efdb
Lang <silentbalanceyh@126.com>
2020-07-13 11:44:17 +0800

    Projection Keep

--- vertx-pin/zero-atom/src/main/java/io/vertx/tp/atom/modeling/element/ZERO.java
+++ vertx-pin/zero-atom/src/main/java/io/vertx/tp/atom/modeling/element/ZERO.java
@@ -22,27 +22,26 @@ class Sync {
                        final Set<String> projection) {
         if (null != record) {
             /* 有数据 */
-            Arrays.asList(record.fields())
-                    .forEach(field -> {
-                        // 抽取表格列
-                        final String column = field.getName();
-                        // 抽取字段名
-                        final String attribute = matrix.getField(column);
-                        // 抽取字段名为空
-                        if (Ut.notNil(attribute)) {
-                            // projection 为空表示不过滤
-                            if (0 == projection.size() || !projection.contains(attribute)) {
-                                // 抽取字段类型
-                                final Class<?> type = matrix.getType(attribute);
-                                // 读值
-                                final Object value = record.getValue(field, type);
-                                // 填充值处理
-                                matrix.set(attribute, value);
-                                // 填充 Record 数据，必须是 JsonObject 可适配类型
-                                recordReference.set(attribute, Ut.aiJValue(value, type));
-                            }
-                        }
-                    });
+            Arrays.asList(record.fields()).forEach(field -> {
+                // 抽取表格列
+                final String column = field.getName();
+                // 抽取字段名
+                final String attribute = matrix.getField(column);
+                // 抽取字段名为空
+                if (Ut.notNil(attribute)) {
+                    // projection 表示传入字段的核心信息
+                    if (0 == projection.size() || projection.contains(attribute)) {
+                        // 抽取字段类型
+                        final Class<?> type = matrix.getType(attribute);
+                        // 读值
+                        final Object value = record.getValue(field, type);
+                        // 填充值处理
+                        matrix.set(attribute, value);
+                        // 填充 Record 数据，必须是 JsonObject 可适配类型
+                        recordReference.set(attribute, Ut.aiJValue(value, type));
+                    }
+                }
+            });
         }
     }
 }
