a7e0272fa0fcddb9cc3d50d04345d1278d80ff55
Lang <silentbalanceyh@126.com>
2020-07-30 15:11:04 +0800

    New Bound

--- vertx-pin/zero-rbac/src/main/java/cn/vertxup/rbac/service/accredit/MatrixService.java
+++ vertx-pin/zero-rbac/src/main/java/cn/vertxup/rbac/service/accredit/MatrixService.java
@@ -3,9 +3,12 @@ package cn.vertxup.rbac.service.accredit;
 import cn.vertxup.rbac.domain.tables.pojos.SResource;
 import cn.vertxup.rbac.service.view.ViewStub;
 import io.vertx.core.Future;
+import io.vertx.core.json.JsonObject;
 import io.vertx.tp.rbac.atom.ScRequest;
 import io.vertx.tp.rbac.refine.Sc;
 import io.vertx.up.commune.config.DataBound;
+import io.vertx.up.unity.Ux;
+import io.vertx.up.util.Ut;
 
 import javax.inject.Inject;
 import java.util.Objects;
@@ -42,6 +45,27 @@ public class MatrixService implements MatrixStub {
                         MatrixFlow.toResult(result)
                 )
                 /* DataBound calculate */
-                .compose(MatrixFlow::toBound);
+                .compose(MatrixFlow::toBound)
+                /* DataBound for calculation of resource here */
+                .compose(bound -> {
+                    final Boolean virtual = resource.getVirtual();
+                    /*
+                     * Check whether current resource is virtual resource
+                     * 1) If true, the resource is virtual resource, there need additional steps
+                     * to calculated view in future instead of current view stored.
+                     * 2) If false, the old workflow
+                     */
+                    if (Objects.nonNull(virtual) && virtual) {
+                        final JsonObject seeker = new JsonObject();
+                        seeker.put("config", Ut.toJObject(resource.getSeekConfig()));
+                        seeker.put("syntax", Ut.toJObject(resource.getSeekSyntax()));
+                        seeker.put("component", resource.getSeekComponent());
+                        /*
+                         * Store view object into json for future condition building
+                         */
+                        bound.addSeeker(seeker);
+                    }
+                    return Ux.future(bound);
+                });
     }
 }
