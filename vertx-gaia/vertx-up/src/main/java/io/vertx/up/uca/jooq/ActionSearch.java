6f33284ef2c1cae64341615527605718ffbd7d3e
Lang <silentbalanceyh@126.com>
2021-03-22 13:16:49 +0800

    Process

--- vertx-gaia/vertx-up/src/main/java/io/vertx/up/uca/jooq/ActionSearch.java
+++ vertx-gaia/vertx-up/src/main/java/io/vertx/up/uca/jooq/ActionSearch.java
@@ -7,6 +7,7 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.up.atom.query.Inquiry;
 import io.vertx.up.eon.Values;
 import io.vertx.up.uca.jooq.util.JqFlow;
+import io.vertx.up.unity.Ux;
 
 /**
  * @author <a href="http://www.origin-x.cn">lang</a>
@@ -27,12 +28,14 @@ class ActionSearch extends AbstractAction {
     <T> Future<JsonObject> searchAsync(final JsonObject query, final JqFlow workflow) {
         final JsonObject response = new JsonObject();
 
+        // before : pojo processing
+        final Inquiry inquiry = workflow.inputQr(query);
+        System.err.println(inquiry.toJson());
         // Search Result
-        final Future<JsonArray> dataFuture = workflow.inputQrAsync(query)  // before : pojo processing
-                .compose(this.qr::searchAsync)                              // execute
+        final Future<JsonArray> dataFuture = this.qr.searchAsync(inquiry)   // execute
                 .compose(workflow::outputAsync);                            // after : pojo processing
         // Count Result
-        final Future<Long> countFuture = this.counter.countAsync(query.getJsonObject(Inquiry.KEY_CRITERIA));
+        final Future<Long> countFuture = this.counter.countAsync(inquiry.getCriteria().toJson());  // execute
 
         return CompositeFuture.join(dataFuture, countFuture).compose(result -> {
             // Processing result
@@ -41,15 +44,17 @@ class ActionSearch extends AbstractAction {
             // Result here
             response.put(FIELD_COUNT, count).put(FIELD_LIST, list);
             return Future.succeededFuture(response);
-        });
+        }).otherwise(Ux.otherwise(new JsonObject()));
     }
 
     <T> JsonObject search(final JsonObject query, final JqFlow workflow) {
         final JsonObject response = new JsonObject();
         // Data Processing
-        final JsonArray list = workflow.output(this.qr.search(workflow.inputQr(query)));
+
+        final Inquiry inquiry = workflow.inputQr(query);
+        final JsonArray list = workflow.output(this.qr.search(inquiry));
         // Count Processing
-        final Long count = this.counter.count(query.getJsonObject(Inquiry.KEY_CRITERIA));
+        final Long count = this.counter.count(inquiry.getCriteria().toJson());
         response.put(FIELD_COUNT, count).put(FIELD_LIST, list);
         return response;
     }
