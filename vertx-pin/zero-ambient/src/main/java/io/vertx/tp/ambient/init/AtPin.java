d0b9ac4ce95f46fb363d432bd2de177594fac5d8
Lang <silentbalanceyh@126.com>
2021-04-07 10:52:39 +0800

    Comments Added

--- vertx-pin/zero-ambient/src/main/java/io/vertx/tp/ambient/init/AtPin.java
+++ vertx-pin/zero-ambient/src/main/java/io/vertx/tp/ambient/init/AtPin.java
@@ -14,13 +14,42 @@ import io.vertx.up.util.Ut;
 
 import java.util.Objects;
 
-/*
- * Init Plugin for 'initAsync' static life
+/**
+ * ## 「Init」AtPin
+ *
+ * ### 1. Intro
+ *
+ * This component could be configured in `vertx-extension.yml` file such as following segment:
+ *
+ * ```yaml
+ * // <pre><code class="yaml">
+ *
+ * init:
+ *     - component: io.vertx.tp.ambient.init.AtPin
+ *
+ * // </code></pre>
+ * ```
+ *
+ * When zero container starting up, the `AtPin.init()` method will be called by framework.
+ *
+ * #### 1.1. init()
+ *
+ * This method will be call in zero framework booting workflow and it will do following phases:
+ *
+ * - Call `AtConfiguration.init()` to initialize the configuration data of current module.
+ * - Call `AtTodo.init()` to initialize the `todo` environment of current module, related to `X_TODO`.
+ *
+ * @author <a href="http://www.origin-x.cn">Lang</a>
  */
 public class AtPin {
-
+    /**
+     * Zero standard logger of {@link io.vertx.up.log.Annal} instance.
+     */
     private static final Annal LOGGER = Annal.get(AtPin.class);
 
+    /**
+     * 「Booting」This method must be defined and it will be called when zero container booting up.
+     */
     public static void init() {
         Ke.banner("「περιβάλλων」- Ambient ( At )");
         At.infoInit(LOGGER, "AtConfiguration...");
@@ -29,32 +58,68 @@ public class AtPin {
         AtTodo.init();
     }
 
+    /**
+     * Return to reference of {@link io.vertx.tp.ambient.atom.AtConfig} instance.
+     *
+     * @return {@link io.vertx.tp.ambient.atom.AtConfig} Deserialized application configuration data.
+     */
     public static AtConfig getConfig() {
         return AtConfiguration.getConfig();
     }
 
+    /**
+     * Return to configuration data that convert to {@link io.vertx.core.json.JsonObject} here by type.
+     *
+     * @param type {@link java.lang.String} The type value passed.
+     *
+     * @return {@link io.vertx.core.json.JsonObject}
+     */
     public static JsonObject getTodo(final String type) {
         return AtTodo.getTodo(type);
     }
 
+    /**
+     * Configuration initialization phase.
+     *
+     * @return {@link io.vertx.tp.optic.extension.Init}
+     */
     public static Init getInit() {
         return getInit(getConfig().getInitializer());
     }
 
+    /**
+     * Data initialization phase.
+     *
+     * @return {@link io.vertx.tp.optic.extension.Init}
+     */
     public static Init getLoader() {
         return getInit(getConfig().getLoader());
     }
 
-    private static Init getInit(final Class<?> initializer) {
-        if (Objects.isNull(initializer)) {
+    /**
+     * The uniform method that could be called private.
+     *
+     * @param initClass {@link java.lang.Class} Input class type that will be reflect to `Init`
+     *
+     * @return {@link io.vertx.tp.optic.extension.Init}
+     * @throws io.vertx.tp.error._500InitSpecificationException Failure then configuration missing.
+     */
+    private static Init getInit(final Class<?> initClass) {
+        if (Objects.isNull(initClass)) {
             return null;
         } else {
-            Fn.outWeb(!Ut.isImplement(initializer, Init.class), _500InitSpecificationException.class,
-                    AtPin.class, initializer.getName());
-            return Init.generate(initializer);
+            Fn.outWeb(!Ut.isImplement(initClass, Init.class), _500InitSpecificationException.class,
+                    AtPin.class, initClass.getName());
+            return Init.generate(initClass);
         }
     }
 
+    /**
+     * Return to `Pre-` component that defined in zero extension modules.
+     *
+     * @return {@link io.vertx.tp.optic.extension.Prerequisite}
+     * @throws io.vertx.tp.error._500PrerequisiteSpecException Failure then configuration missing.
+     */
     public static Prerequisite getPrerequisite() {
         final Class<?> prerequisite = getConfig().getPrerequisite();
         if (Objects.isNull(prerequisite)) {
