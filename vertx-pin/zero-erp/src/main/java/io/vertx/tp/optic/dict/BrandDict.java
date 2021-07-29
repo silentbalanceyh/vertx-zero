d077e11589d21ca588981596fe28c1f61d1b49bd
zwkjhx <1264210109@qq.com>
2021-07-29 16:13:37 +0800

    品牌BrandDict

---
+++ vertx-pin/zero-erp/src/main/java/io/vertx/tp/optic/dict/BrandDict.java
@@ -0,0 +1,30 @@
+package io.vertx.tp.optic.dict;
+
+import cn.vertxup.erp.domain.tables.daos.EBrandDao;
+import io.vertx.core.Future;
+import io.vertx.core.MultiMap;
+import io.vertx.core.json.JsonArray;
+import io.vertx.tp.ke.cv.KeField;
+import io.vertx.tp.optic.component.DictionaryPlugin;
+import io.vertx.up.commune.exchange.DictSource;
+import io.vertx.up.unity.Ux;
+import io.vertx.up.util.Ut;
+
+/*
+ * Dict for `resource.departments` here
+ */
+public class BrandDict implements DictionaryPlugin {
+
+    @Override
+    public Future<JsonArray> fetchAsync(final DictSource source,
+                                        final MultiMap paramMap) {
+        final String sigma = paramMap.get(KeField.SIGMA);
+        if (Ut.notNil(sigma)) {
+            return Ux.Jooq.on(EBrandDao.class)
+                    .fetchAsync(KeField.SIGMA, sigma)
+                    .compose(Ux::futureA);
+        } else {
+            return Ux.future(new JsonArray());
+        }
+    }
+}
