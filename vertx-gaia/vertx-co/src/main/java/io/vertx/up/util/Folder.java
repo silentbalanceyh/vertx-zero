0e80304f86ba4d6c559bb298bd4244900dd31096
zwkjhx <1264210109@qq.com>
2021-08-20 17:50:41 +0800

    元数据仓库初始化导入修改

--- vertx-gaia/vertx-co/src/main/java/io/vertx/up/util/Folder.java
+++ vertx-gaia/vertx-co/src/main/java/io/vertx/up/util/Folder.java
@@ -61,9 +61,9 @@ final class Folder {
                 final String[] folderList = folderObj.list();
                 assert folderList != null;
                 Arrays.stream(folderList).forEach(folderS -> {
-                    String relatedPath = folderObj.getAbsolutePath();
-                    relatedPath = relatedPath.replace(root, Strings.EMPTY);
-                    folders.addAll(listDirectoriesN(relatedPath + "/" + folderS, root));
+//                    String relatedPath = folderObj.getAbsolutePath();
+//                    relatedPath = relatedPath.replace(root, Strings.EMPTY);
+                    folders.addAll(listDirectoriesN(folder + folderS, root));
                 });
             }
         }
