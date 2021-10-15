7b582484ec3c21e49086efdf062712f4681501e3
Lang <silentbalanceyh@126.com>
2021-10-15 18:02:16 +0800

    File System

--- vertx-pin/zero-ambient/src/main/resources/plugin/sql/ambient/X_ATTACHMENT.sql
+++ vertx-pin/zero-ambient/src/main/resources/plugin/sql/ambient/X_ATTACHMENT.sql
@@ -16,6 +16,7 @@ CREATE TABLE IF NOT EXISTS X_ATTACHMENT
     `MODULE`     VARCHAR(64) COMMENT '「module」- 业务标识',
     `MIME`       VARCHAR(64) COMMENT '「mime」- 该文件的MIME类型',
     `SIZE`       INTEGER COMMENT '「size」- 该文件的尺寸',
+    -- 文件管理专用
 
     -- 特殊字段
     `ACTIVE`     BIT         DEFAULT NULL COMMENT '「active」- 是否启用',
