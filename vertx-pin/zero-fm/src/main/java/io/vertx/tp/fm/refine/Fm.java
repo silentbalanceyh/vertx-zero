81d777af957bdb475a1924dbeda9cff3bbc0a434
Lang <silentbalanceyh@126.com>
2021-10-18 20:21:37 +0800

    Book Service

--- vertx-pin/zero-fm/src/main/java/io/vertx/tp/fm/refine/Fm.java
+++ vertx-pin/zero-fm/src/main/java/io/vertx/tp/fm/refine/Fm.java
@@ -1,5 +1,11 @@
 package io.vertx.tp.fm.refine;
 
+import cn.vertxup.fm.domain.tables.pojos.FBook;
+import io.vertx.core.json.JsonObject;
+import io.vertx.tp.ke.atom.KSpec;
+
+import java.util.List;
+
 /**
  * @author <a href="http://www.origin-x.cn">Lang</a>
  */
@@ -7,7 +13,25 @@ public final class Fm {
     private Fm() {
     }
 
-    /*
-     * umCreated
-     */
+    public static JsonObject qrBook(final KSpec spec) {
+        return FmBook.qrBook(spec);
+    }
+
+    public static FBook umBook(final KSpec spec) {
+        return FmBook.umBook(spec);
+    }
+
+    public static List<FBook> umBook(final KSpec spec, final List<FBook> books) {
+        return FmBook.umBook(spec, books);
+    }
+
+    public static class Log {
+        public static void infoBook(final Class<?> clazz, final String pattern, final Object... args) {
+            FmLog.info(clazz, "Book", pattern, args);
+        }
+
+        public static void warnBook(final Class<?> clazz, final String pattern, final Object... args) {
+            FmLog.warn(clazz, "Book", pattern, args);
+        }
+    }
 }
