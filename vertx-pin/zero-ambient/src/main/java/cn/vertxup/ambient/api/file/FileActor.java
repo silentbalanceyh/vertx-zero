f2e633367532226bc26d9261baa70b990d615672
Lang <silentbalanceyh@126.com>
2021-11-23 19:36:06 +0800

    module Processing

--- vertx-pin/zero-ambient/src/main/java/cn/vertxup/ambient/api/file/FileActor.java
+++ vertx-pin/zero-ambient/src/main/java/cn/vertxup/ambient/api/file/FileActor.java
@@ -1,11 +1,15 @@
 package cn.vertxup.ambient.api.file;
 
+import cn.vertxup.ambient.domain.tables.daos.XAttachmentDao;
 import io.vertx.core.Future;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.auth.User;
 import io.vertx.tp.ambient.cv.Addr;
+import io.vertx.tp.ke.cv.em.FileStatus;
 import io.vertx.up.annotations.Address;
 import io.vertx.up.annotations.Queue;
+import io.vertx.up.eon.KName;
+import io.vertx.up.unity.Ux;
 
 /**
  * @author <a href="http://www.origin-x.cn">Lang</a>
@@ -17,13 +21,17 @@ public class FileActor {
     @Address(Addr.File.MY_QUEUE)
     public Future<JsonObject> searchMy(final JsonObject query,
                                        final User user) {
-
-        return null;
+        // JsonObject on `my queue` criteria
+        final JsonObject qrDefault = Ux.whereAnd();
+        qrDefault.put(KName.STATUS, FileStatus.DONE.name());
+        qrDefault.put(KName.ACTIVE, Boolean.TRUE);
+        qrDefault.put(KName.CREATED_BY, Ux.keyUser(user));
+        final JsonObject qrCombine = Ux.whereQrA(query, "$DFT$", qrDefault);
+        return Ux.Jooq.on(XAttachmentDao.class).searchAsync(qrCombine);
     }
 
     @Address(Addr.File.BY_KEY)
     public Future<JsonObject> fileByKey(final String key) {
-
-        return null;
+        return Ux.Jooq.on(XAttachmentDao.class).fetchJByIdAsync(key);
     }
 }
