0fb8cb4fe8a458b8fb14154faf868d6d7bbf7922
Lang <silentbalanceyh@126.com>
2022-02-21 18:54:28 +0800

    metadata field

--- vertx-pin/zero-ambient/src/main/java/io/vertx/tp/optic/business/ExAttachment.java
+++ vertx-pin/zero-ambient/src/main/java/io/vertx/tp/optic/business/ExAttachment.java
@@ -5,8 +5,10 @@ import cn.vertxup.ambient.domain.tables.pojos.XAttachment;
 import io.vertx.core.Future;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.up.eon.KName;
 import io.vertx.up.uca.jooq.UxJooq;
 import io.vertx.up.unity.Ux;
+import io.vertx.up.util.Ut;
 
 import java.util.List;
 
@@ -16,12 +18,14 @@ import java.util.List;
 public class ExAttachment implements ExFile {
     @Override
     public Future<JsonArray> createAsync(final JsonArray data) {
+        Ut.ifStrings(data, KName.METADATA);
         final List<XAttachment> attachments = Ux.fromJson(data, XAttachment.class);
         return Ux.Jooq.on(XAttachmentDao.class).insertJAsync(attachments);
     }
 
     @Override
     public Future<JsonArray> saveAsync(final JsonObject condition, final JsonArray data) {
+        Ut.ifStrings(data, KName.METADATA);
         final List<XAttachment> attachments = Ux.fromJson(data, XAttachment.class);
         final UxJooq jq = Ux.Jooq.on(XAttachmentDao.class);
         return jq.deleteByAsync(condition).compose(nil -> jq.insertJAsync(attachments));
@@ -29,6 +33,7 @@ public class ExAttachment implements ExFile {
 
     @Override
     public Future<JsonArray> fetchAsync(final JsonObject condition) {
-        return Ux.Jooq.on(XAttachmentDao.class).fetchJAsync(condition);
+        return Ux.Jooq.on(XAttachmentDao.class).fetchJAsync(condition)
+            .compose(Ut.ifJArray(KName.METADATA));
     }
 }
