cc15adec162eda6e19d76e0ea012c1683114e6d0
Lang <silentbalanceyh@126.com>
2022-03-05 07:10:34 +0800

    Integration for Open Source

--- vertx-pin/zero-is/src/main/resources/plugin/sql/is/I_INTEGRATION.sql
+++ vertx-pin/zero-is/src/main/resources/plugin/sql/is/I_INTEGRATION.sql
@@ -5,23 +5,26 @@
 DROP TABLE IF EXISTS I_INTEGRATION;
 CREATE TABLE IF NOT EXISTS I_INTEGRATION
 (
+    -- 基本信息
     `KEY`             VARCHAR(36) COMMENT '「key」- 集成配置主键',
     `NAME`            VARCHAR(255) COMMENT '「name」- 集成名称',
     `TYPE`            VARCHAR(255) COMMENT '「type」- 集成类型',
 
-    -- 基础配置信息
+    -- 网络信息
     `IP_V4`           VARCHAR(15) COMMENT '「ipV4」- IP v4地址',
     `IP_V6`           VARCHAR(40) COMMENT '「ipV6」- IP v6地址',
     `HOSTNAME`        VARCHAR(255) COMMENT '「hostname」- 主机地址',
-    `ENDPOINT`        VARCHAR(255) COMMENT '「endpoint」- 端地址',
 
     `PORT`            INT COMMENT '「port」- 端口号',
     `PROTOCOL`        VARCHAR(64) COMMENT '「protocol」- 协议类型：HTTP, HTTPS, FTP',
+
+    -- 特殊协议的安全相关信息
     `SECURE_PORT`     INT COMMENT '「securePort」- 传输层安全接口',
     `SECURE_PROTOCOL` VARCHAR(32) COMMENT '「secureProtocol」- 传入层协议：TLS / SSL（邮件服务器需要）',
 
-    -- FTP需要使用
+    `ENDPOINT`        VARCHAR(255) COMMENT '「endpoint」- 端地址',
     `PATH`            VARCHAR(255) COMMENT '「path」- 集成专用根路径',
+
     `OS_KEY`          NVARCHAR(1024) COMMENT '「osKey」- 开源专用Key',
     `OS_SECRET`       NVARCHAR(1024) COMMENT '「osSecret」- 开源专用Secret',
     `OS_AUTHORIZE`    VARCHAR(255) COMMENT '「osAuthorize」- Authorize接口',
@@ -30,6 +33,7 @@ CREATE TABLE IF NOT EXISTS I_INTEGRATION
     `USERNAME`        VARCHAR(255) COMMENT '「username」- 账号',
     `PASSWORD`        VARCHAR(255) COMMENT '「password」- 密码',
     `PUBLIC_KEY`      VARCHAR(255) COMMENT '「publicKey」- Key文件',
+
     `OPTIONS`         TEXT COMMENT '「options」- 集成相关配置',
 
     -- 特殊属性
