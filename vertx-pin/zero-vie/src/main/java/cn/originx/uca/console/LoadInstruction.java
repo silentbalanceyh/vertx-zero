cb67cb42c83f46dfd4e618eb53273ebb1dab2728
Lang <silentbalanceyh@126.com>
2022-03-29 11:22:31 +0800

    Load

--- vertx-pin/zero-vie/src/main/java/cn/originx/uca/console/LoadInstruction.java
+++ vertx-pin/zero-vie/src/main/java/cn/originx/uca/console/LoadInstruction.java
@@ -9,6 +9,8 @@ import io.vertx.tp.plugin.shell.cv.em.TermStatus;
 import io.vertx.tp.plugin.shell.refine.Sl;
 import io.vertx.up.unity.Ux;
 
+import java.util.Objects;
+
 /**
  * @author <a href="http://www.origin-x.cn">Lang</a>
  */
@@ -16,7 +18,14 @@ public class LoadInstruction extends AbstractInstruction {
     @Override
     public Future<TermStatus> executeAsync(final CommandInput args) {
         final Boolean isOob = this.inBoolean(args, "o");
-        return Ok.app().compose(ok -> Bt.initAsync("init/oob/", isOob).compose(done -> {
+        // Fix Issue
+        final boolean oob;
+        if (Objects.isNull(isOob)) {
+            oob = false;
+        } else {
+            oob = isOob;
+        }
+        return Ok.app().compose(ok -> Bt.initAsync("init/oob/", oob).compose(done -> {
             Sl.output("您的元数据仓库已重置初始化完成！重置结果：{0}", done);
             return Ux.future(done ? TermStatus.SUCCESS : TermStatus.FAILURE);
         }));
