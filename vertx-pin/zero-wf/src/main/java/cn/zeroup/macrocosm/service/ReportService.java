c74bb535ed04f934b3b616b8efbef409493e9df0
Robert Min <jcm007@live.cn>
2022-06-07 22:00:01 +0800

    fix(report): fixed bug codes in ReportService.java

--- vertx-pin/zero-wf/src/main/java/cn/zeroup/macrocosm/service/ReportService.java
+++ vertx-pin/zero-wf/src/main/java/cn/zeroup/macrocosm/service/ReportService.java
@@ -1,31 +1,15 @@
 package cn.zeroup.macrocosm.service;
 
-import cn.vertxup.ambient.domain.tables.pojos.XActivity;
 import cn.vertxup.ambient.domain.tables.daos.XActivityDao;
-import cn.vertxup.workflow.domain.tables.daos.TAssetOutDao;
 import cn.vertxup.workflow.domain.tables.daos.WTicketDao;
 import cn.vertxup.workflow.domain.tables.daos.WTodoDao;
-import cn.vertxup.workflow.domain.tables.pojos.WTicket;
-import cn.vertxup.workflow.domain.tables.pojos.WTodo;
-import cn.zeroup.macrocosm.cv.WfMsg;
 import io.vertx.core.Future;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
-import io.vertx.ext.auth.User;
-import io.vertx.tp.optic.feature.Todo;
-import io.vertx.tp.workflow.atom.EngineOn;
-import io.vertx.tp.workflow.atom.MetaInstance;
-import io.vertx.tp.workflow.atom.WRecord;
-import io.vertx.tp.workflow.refine.Wf;
-import io.vertx.tp.workflow.uca.component.AidLinkage;
 import io.vertx.up.eon.KName;
-import io.vertx.up.uca.jooq.UxJooq;
 import io.vertx.up.unity.Ux;
-import io.vertx.up.util.Ut;
 
 import javax.inject.Inject;
-import java.util.List;
-import java.util.Objects;
 
 /**
  * @author <a href="http://www.origin-x.cn">Lang</a>
@@ -55,19 +39,20 @@ public class ReportService implements ReportStub {
     @Override
     public Future<JsonArray> fetchActivity(final String key, final String modelKey) {
         return Ux.Jooq.on(XActivityDao.class)
-                .fetchAndAsync(new JsonObject().put("modelKey", modelKey))
-                .compose(Ux::futureA)
-                ;
+            .fetchAndAsync(new JsonObject().put("modelKey", modelKey))
+            .compose(Ux::futureA)
+            ;
     }
+
     @Override
     public Future<JsonArray> fetchActivityByUser(final String key, final String modelKey, final String userId) {
         return Ux.Jooq.on(XActivityDao.class)
-                .fetchAndAsync(
-                        new JsonObject()
-                                .put("modelKey", modelKey)
-                                .put("createdBy",userId)
-                )
-                .compose(Ux::futureA)
-                ;
+            .fetchAndAsync(
+                new JsonObject()
+                    .put("modelKey", modelKey)
+                    .put("createdBy", userId)
+            )
+            .compose(Ux::futureA)
+            ;
     }
 }
