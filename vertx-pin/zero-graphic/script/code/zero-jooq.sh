ca48ad5f763f92bd0f21a89fe3b09d03ec265a61
Lang <silentbalanceyh@126.com>
2022-06-23 10:49:59 +0800

    zero-graphic

--- vertx-pin/zero-graphic/script/code/zero-jooq.sh
+++ vertx-pin/zero-graphic/script/code/zero-jooq.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-java -classpath jooq-3.15.3.jar:jooq-meta-3.15.3.jar:jooq-codegen-3.15.3.jar:mysql-connector-java-8.0.11.jar:reactive-streams-1.0.3.jar:vertx-jooq-generate-6.3.0.jar:vertx-jooq-shared-6.3.0.jar:vertx-jooq-classic-6.3.0.jar:jaxb-api-2.4.0-b180830.0359.jar:r2dbc-spi-0.8.6.RELEASE.jar:vertx-core-4.1.4.jar \
+java -classpath jooq-3.16.7.jar:jooq-meta-3.16.7.jar:jooq-codegen-3.16.7.jar:mysql-connector-java-8.0.11.jar:reactive-streams-1.0.4.jar:vertx-jooq-generate-6.5.3.jar:vertx-jooq-shared-6.5.3.jar:vertx-jooq-classic-6.5.3.jar:jakarta.xml.bind-api-4.0.0.jar:r2dbc-spi-1.0.0.RELEASE.jar:vertx-core-4.3.1.jar \
   org.jooq.codegen.GenerationTool ./config/zero-jooq.xml
