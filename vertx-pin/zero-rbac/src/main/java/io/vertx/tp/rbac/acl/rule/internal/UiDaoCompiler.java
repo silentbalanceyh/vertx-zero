09b534b36a1d91b84bfd934fa554222ccada6c37
Lang <silentbalanceyh@126.com>
2022-08-14 13:19:27 +0800

    Menu Finished

--- vertx-pin/zero-rbac/src/main/java/io/vertx/tp/rbac/acl/rule/internal/UiDaoCompiler.java
+++ vertx-pin/zero-rbac/src/main/java/io/vertx/tp/rbac/acl/rule/internal/UiDaoCompiler.java
@@ -3,6 +3,13 @@ package io.vertx.tp.rbac.acl.rule.internal;
 import io.vertx.core.Future;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.tp.error._404AdmitDaoNullException;
+import io.vertx.up.eon.KName;
+import io.vertx.up.fn.Fn;
+import io.vertx.up.unity.Ux;
+import io.vertx.up.util.Ut;
+
+import java.util.Objects;
 
 /**
  * @author <a href="http://www.origin-x.cn">Lang</a>
@@ -10,6 +17,11 @@ import io.vertx.core.json.JsonObject;
 public class UiDaoCompiler implements HAdmitCompiler {
     @Override
     public Future<JsonArray> ingest(final JsonObject qr, final JsonObject config) {
-        return null;
+        final String daoStr = Ut.valueString(config, KName.DAO);
+        final Class<?> daoCls = Ut.clazz(daoStr, null);
+
+        // Error-80226, uiConfig中没有配置dao节点
+        Fn.out(Objects.isNull(daoCls), _404AdmitDaoNullException.class, this.getClass(), daoStr);
+        return Ux.Jooq.on(daoCls).fetchJAsync(qr);
     }
 }
