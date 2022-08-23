85d2dd6133a73c56031726381ade34f123a91fe5
Lang <silentbalanceyh@126.com>
2022-08-23 12:30:40 +0800

    Junc

--- vertx-pin/zero-rbac/src/main/java/io/vertx/tp/rbac/acl/relation/Junc.java
+++ vertx-pin/zero-rbac/src/main/java/io/vertx/tp/rbac/acl/relation/Junc.java
@@ -16,12 +16,18 @@ public class Junc {
 
     private static final Cc<String, Tie> CC_TIE = Cc.openThread();
 
-    /* ExUser 内置调用，处理 modelId / modelKey 专用 */
+    /*
+     * ExUser 内置调用，处理 modelId / modelKey 专用
+     * -- 旧版本使用 modelId / modelKey 更新 S_USER 专用
+     **/
     public static Twine<String> refModel() {
         return (Twine<String>) CC_TWINE.pick(TwineModel::new, TwineModel.class.getName());
     }
 
-    /* 原 UserExtension，根据配置文件处理专用操作用户和其他账号的链接 */
+    /*
+     * 原 UserExtension，根据配置文件处理专用操作用户和其他账号的链接
+     * -- 新版本使用 S_USER 和配置中对应类型的账号相关联专用
+     * */
     public static Twine<SUser> refExtension() {
         return (Twine<SUser>) CC_TWINE.pick(TwineExtension::new, TwineExtension.class.getName());
     }
