ba9c1ad6b83fbafb0889ab01416567bd22da34fa
Lang <silentbalanceyh@126.com>
2022-09-02 11:06:46 +0800

    Workflow Management

--- vertx-pin/zero-rbac/src/main/java/io/vertx/tp/rbac/ruler/HSDimDao.java
+++ vertx-pin/zero-rbac/src/main/java/io/vertx/tp/rbac/ruler/HSDimDao.java
@@ -8,7 +8,14 @@ import io.vertx.core.json.JsonObject;
 public class HSDimDao extends HSDimNorm {
     @Override
     protected Future<JsonArray> compile(final HPermit input, final Class<?> daoCls, final JsonObject config) {
-        // Dao 专用维度转换器，执行读取构造新的维度数据用于菜单
+        /*
+         * Dao 专用维度转换器，执行读取构造新的维度数据用于菜单
+         * 输出格式:
+         * -- key
+         * -- label
+         * -- value
+         * -- data 这个节点是非静态模式必须带的数据节点，用于前端做维度计算
+         */
         return super.compile(input, daoCls, config);
     }
 }
