378de5f129edd955a23f4b930d4f0a553892574b
62743 <627433532@qq.com>
2022-12-29 17:20:33 +0800

    新增:租借管理

--- zero-compile.bat
+++ zero-compile.bat
@@ -1,1 +1,1 @@
-mvnd clean package install -DskipTests=true '-Dmaven.javadoc.skip=true' '-Dmaven.compile.fork=true' -T 1C
+mvnd clean package install -DskipTests=true -Dmaven.javadoc.skip=true -Dmaven.compile.fork=true -T 1C
