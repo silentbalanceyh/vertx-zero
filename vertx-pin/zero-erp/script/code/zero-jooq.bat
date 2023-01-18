307d2beafa1a164bd03591068cebf143e57c55c7
62743 <627433532@qq.com>
2023-01-18 10:02:33 +0800

    夜审后，房间自动变更为住脏房

---
+++ vertx-pin/zero-erp/script/code/zero-jooq.bat
@@ -0,0 +1,17 @@
+java -classpath jooq-3.17.6.jar;^
+jooq-meta-3.17.6.jar;^
+jooq-codegen-3.17.6.jar;^
+reactive-streams-1.0.4.jar;^
+r2dbc-spi-1.0.0.RELEASE.jar;^
+jakarta.xml.bind-api-4.0.0.jar;^
+vertx-jooq-generate-6.5.5.jar;^
+vertx-core-4.3.3.jar;^
+vertx-jooq-classic-6.5.5.jar;^
+vertx-jooq-shared-6.5.5.jar;^
+mysql-connector-j-8.0.31.jar;. ^
+org.jooq.codegen.GenerationTool zero-jooq.xml
+
+
+
+
+
