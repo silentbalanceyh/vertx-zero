056e3cec7cef35db555179997333bb8e0493c3b2
silentbalanceyh <silentbalanceyh@126.com>
2023-03-14 17:33:51 +0800

    新版菜单管理

--- vertx-gaia/vertx-tp/src/main/java/io/vertx/tp/plugin/database/HikariDataPool.java
+++ vertx-gaia/vertx-tp/src/main/java/io/vertx/tp/plugin/database/HikariDataPool.java
@@ -60,9 +60,9 @@ public class HikariDataPool implements DataPool {
             this.dataSource.setAutoCommit(autoCommit);
             // Fix Issue: https://github.com/silentbalanceyh/vertx-zero/issues/304
             this.dataSource.setConnectionTimeout(this.database.getLong(OPT_CONNECTION_TIMEOUT, 300000L));
-            this.dataSource.setIdleTimeout(this.database.getLong(OPT_IDLE_TIMEOUT, 450000L));
+            this.dataSource.setIdleTimeout(this.database.getLong(OPT_IDLE_TIMEOUT, 60000L));
             // Fix: Possibly consider using a shorter maxLifetime value
-            this.dataSource.setMaxLifetime(this.database.getLong(OPT_MAX_LIFETIME, 900000L));
+            this.dataSource.setMaxLifetime(this.database.getLong(OPT_MAX_LIFETIME, 120000L));
             this.dataSource.setMinimumIdle(this.database.getOption(OPT_MINIMUM_IDLE, 0));
             this.dataSource.setMaximumPoolSize(this.database.getOption(OPT_MAXIMUM_POOL_SIZE, 2048));
 
